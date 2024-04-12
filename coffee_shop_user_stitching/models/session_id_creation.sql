23a44c90c6766fc15e56104f74220681fbb35daf
Katie Hilton <98290598+hiltonkm@users.noreply.github.com>
2024-04-12 20:16:19 +0000

    adding session summary view

--- coffee_shop_user_stitching/models/session_id_creation.sql
+++ coffee_shop_user_stitching/models/session_id_creation.sql
@@ -12,10 +12,23 @@ session_flags as (select *
   , timestamp_diff(timestamp, last_session, MINUTE) as time_diff
   , case when  timestamp_diff(timestamp, last_session, MINUTE) >= 30 or last_session is null then 1
     else 0 end as new_session_flag
-from last)
+from last),
 
 -- Creating ID by summing flags (ordering by visitor_id's and time)
+ids as (
+    select 
+        sum(new_session_flag) over (order by visitor_id, timestamp) as session_id
+        , *
+    from session_flags)
+
+-- Creating session start time and end time
 select 
-    sum(new_session_flag) over (order by visitor_id, timestamp) as session_id
-    , *
-from session_flags
+    session_id
+    , visitor_id
+    , customer_id
+    , device_type
+    , page
+    , timestamp
+    , first_value(timestamp) over (partition by session_id order by timestamp) as session_start_time
+    , first_value(timestamp) over (partition by session_id order by timestamp desc) as session_end_time
+from ids
